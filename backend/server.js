a5d54c2469ed0a4240854909ee5e434b4c50fb63
Zain Saleem <zainsaleem022@gmail.com>
2024-05-14 17:48:40 +0500

    setup updated

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
 const express = require("express");
+const { chats } = require("./data/data");
+const dotenv = require("dotenv");
+const connectDB = require("./database.js");
+const colors = require("colors");
+const userRoutes = require("./routes/Routes.js");
+const {
+  notFound,
+  errorHandler,
+} = require("./middleware/errorHandlerMiddleware");
