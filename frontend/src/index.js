2294c8985d8e7296b6c4bf975993067efb28b595
Zain Saleem <zainsaleem022@gmail.com>
2024-05-14 22:54:50 +0500

    another one

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import BfitNavbar from "./components/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <>
+    <BfitNavbar />
+    <App />
+  </>
+);
