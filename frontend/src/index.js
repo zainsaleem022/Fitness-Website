c5d92defb64516f8125216f07a492cbb73df8950
Ahmed Abdur Rahman <asquarer02@yahoo.com>
2024-05-14 23:52:03 +0500

    risky commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,15 +7,15 @@ import ResponsiveAppBar from "./components/Navbar";
 import ExerciseLibrary from "./pages/ExerciseLibrary";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <>
-    <BfitNavbar />
-    <App />
-  </>
-);
+// root.render(
+//   <>
+//     <BfitNavbar />
+//     <App />
+//   </>
+// );
 root.render(
     <>
-      <ResponsiveAppBar />
+      <BfitNavbar />
       <ExerciseLibrary />
       {/* <App /> */}
     </>
