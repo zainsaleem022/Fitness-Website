fb0d21cc5e8782d745866c0d7fc98b67a63afa73
Zain Saleem <zainsaleem022@gmail.com>
2024-05-16 10:05:23 +0500

    error handler

---
+++ backend/middleware/errorHandlerMiddleware.js
@@ -0,0 +1,18 @@
+// Middleware function for handling 404 Not Found errors
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+// Middleware function for handling all other errors
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
+  });
+};
+
+module.exports = { notFound, errorHandler };
--- backend/server.js
+++ backend/server.js
@@ -18,67 +18,12 @@ app.use(cors());
 // Middleware
 app.use(express.json()); //to accept JSON data
 
-// // Define dummy data
-// const dummyWorkouts = [
-//   {
-//     exercise: "Push-ups",
-//     weight: 0, // If it's bodyweight exercise, weight could be 0
-//     sets: 3,
-//     reps: 15,
-//     dayOfWeek: "Monday",
-//     type_: "goal", // or "goal" depending on your scenario
-//   },
-//   {
-//     exercise: "Squats",
-//     weight: 50, // If using weights, specify the weight
-//     sets: 4,
-//     reps: 12,
-//     dayOfWeek: "Monday",
-//     type_: "goal",
-//   },
-//   {
-//     exercise: "Pull-ups",
-//     weight: 10, // If it's bodyweight exercise, weight could be 0
-//     sets: 3,
-//     reps: 15,
-//     dayOfWeek: "Wednesday",
-//     type_: "goal", // or "goal" depending on your scenario
-//   },
-//   {
-//     exercise: "Weighted-Squats",
-//     weight: 10, // If using weights, specify the weight
-//     sets: 4,
-//     reps: 12,
-//     dayOfWeek: "Wednesday",
-//     type_: "goal",
-//   },
-//   // Add more dummy data as needed
-// ];
-
-// // Function to insert dummy data into the database
-// const insertDummyData = async () => {
-//   try {
-//     // Clear existing data (optional)
-//     await Workout.deleteMany({});
-
-//     // Insert dummy data
-//     await Workout.insertMany(dummyWorkouts);
-
-//     console.log("Dummy data inserted successfully");
-//   } catch (error) {
-//     console.error("Error inserting dummy data:", error);
-//   }
-// };
-
-// // Call the function to insert dummy data
-// insertDummyData();
-
 // Routes
 app.use("/bfit", userRoutes);
 
 // Error handling middleware
-//app.use(notFound);
-//app.use(errorHandler);
+app.use(notFound);
+app.use(errorHandler);
 
 // Start the server
 app.listen(PORT, () => {
